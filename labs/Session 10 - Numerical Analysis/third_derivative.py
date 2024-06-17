4c8847f11c23e58b76602b5ae3a87bebd6edfc0b
Dave Biersach <dbiersach@gmail.com>
2024-06-17 15:46:26 -0400

    Clarify centered difference equations

--- labs/Session 10 - Numerical Analysis/third_derivative.py
+++ labs/Session 10 - Numerical Analysis/third_derivative.py
@@ -13,26 +13,24 @@ def f(x):
 
 def main():
     a, b, n = 0, 5, 500
-
+    h = (b - a) / n
     x = np.linspace(a, b, n)
-    h = 2 * (b - a) / n
-
     y = f(x)
 
     y_prime = np.zeros_like(y)
     for i in range(1, len(y) - 1):
-        y_prime[i] = (y[i + 1] - y[i - 1]) / h
+        y_prime[i] = (y[i + 1] - y[i - 1]) / (2 * h)
 
     y_prime2 = np.zeros_like(y)
     for i in range(2, len(y_prime2) - 2):
-        y_prime2[i] = (y_prime[i + 1] - y_prime[i - 1]) / h
+        y_prime2[i] = (y_prime[i + 1] - y_prime[i - 1]) / (2 * h)
 
     y_prime3 = np.zeros_like(y)
     for i in range(3, len(y_prime3) - 3):
-        y_prime3[i] = (y_prime2[i + 1] - y_prime2[i - 1]) / h
+        y_prime3[i] = (y_prime2[i + 1] - y_prime2[i - 1]) / (2 * h)
 
     plt.switch_backend("TkAgg")
-    plt.figure(Path(__file__).name)    
+    plt.figure(Path(__file__).name)
     plt.plot(x, 20 * y, label=r"$20\times\frac{\sin{x^2}}{1+x^3}$")
     plt.plot(
         x[1:-2],
